2f997ffe100cdb19e0342fe7345c2d754cafc7b1
cprom <chen.prom@gmail.com>
2020-02-03 00:39:01 -0800

    current weather logic

--- assets/app.js
+++ assets/app.js
@@ -1,1 +1,42 @@
-console.log("hello")
+
+
+//fetch request for main weather info
+fetch("http://api.openweathermap.org/data/2.5/weather?q=dallas&&units=imperial&appid=25b4466b181066c6d16ec93bc2dfa591")
+
+  .then(r => r.json())
+  .then(weather => {
+    console.log(weather)
+    let icon = weather.weather[0].icon
+    let iconUrl = "http://openweathermap.org/img/w/" + icon + ".png"
+    document.getElementById('current-icon').src = iconUrl
+    document.getElementById('current-city').innerHTML = weather.name
+    document.getElementById('temp').innerHTML = "Temperature: " + Math.floor(weather.main.temp) + " " + String.fromCharCode(176) + "F"
+    document.getElementById('humidity').innerHTML = "Humidity: " + weather.main.humidity + "%"
+
+    //get lat/lon to use in retreiving UV index info
+    let lat = weather.coord.lat
+    let lon = weather.coord.lon
+    console.log(lat)
+    console.log(lon)
+
+    fetch(`http://api.openweathermap.org/data/2.5/uvi?appid=25b4466b181066c6d16ec93bc2dfa591&lat=${lat}&lon=${lon}`)
+      .then(r => r.json())
+      .then(uv => {
+        console.log(uv)
+        document.getElementById('uv').textContent = "UV Index: " + uv.value
+      })
+
+
+
+
+
+  })
+
+
+
+  .catch(e => console.error(e))
+
+
+
+
+
